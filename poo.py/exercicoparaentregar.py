1040482ad32436fe309bbb5754f8a79edd6d00af
JOÃO GABRIEL CORREA <joao.correa@alunos.sc.senac.br>
2022-09-05 16:36:30 -0300

    Novos exercicios

--- poo.py/exercicoparaentregar.py
+++ poo.py/exercicoparaentregar.py
@@ -39,12 +39,29 @@ class Banco(ABC):
 
 
 class Pessoa:
+
     def __init__(self,nome,idade,sexo,raca) -> None:
-        self.nome=nome
-        self.idade=idade
+        self.__nome=nome
+        self.__idade=idade
         self.sexo=sexo
         self.raca=raca
         pass
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self,nome):
+            self.__nome=nome
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @idade.setter
+    def idade(self,idade):
+        self.__idade=idade
 
 class Cliente(Pessoa):
     def __init__(self, nome, idade, sexo, raca) -> None:
