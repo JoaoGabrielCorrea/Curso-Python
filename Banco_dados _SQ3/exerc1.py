80ce6763258eeb1f521e9714f70ac9a803486238
JOÃO GABRIEL CORREA <joao.correa@alunos.sc.senac.br>
2022-09-05 15:09:44 -0300

    Novos exercicios

---
+++ Banco_dados _SQ3/exerc1.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conexao=sqlite3.connect("Lista_de_clientes")
+
+comand=conexao.cursor()
+
+comand.execute('CREATE TABLE IF NOT EXISTS clientes('
+'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+'IDI INTEGER,'
+'NOME TEXT,'
+'SALDO REAL,'
+'NUMERO_CONTA INTEGER,'
+'AGENCIA INTEGER'')')
+
+comand.execute('INSERT INTO clientes(IDI,NOME,SALDO,NUMERO_CONTA,AGENCIA) VALUES("11","JOAO",100,001,12)')
+conexao.commit()
+comand.execute('SELECT * FROM clientes')
+for linha in comand.fetchall():
+    print(linha)
