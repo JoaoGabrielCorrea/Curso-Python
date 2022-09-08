ccdfdb67a3ab5377f918b723d381b76c07a42f2c
JOÃO GABRIEL CORREA <joao.correa@alunos.sc.senac.br>
2022-09-08 16:46:24 -0300

    Novos exercicios

--- Banco_dados _SQ3/exerc3.py
+++ Banco_dados _SQ3/exerc3.py
@@ -15,22 +15,35 @@ comand.execute('CREATE TABLE IF NOT EXISTS Produtos('
 'Produto TEXT,'
 'Preco REAL'')')
 
+escolha= (input("Insira sua escolha: 1- Adicionar 2- EDITAR 3-DELETAR "))
 
-produto= (input("Insira nome produto: "))
-preco= (input("Insira preço do produto: "))
+if escolha=="1":
+    produto= (input("Insira nome produto: "))
+    preco= (input("Insira preço do produto: "))
 
-comand.execute(f'INSERT INTO Produtos(Produto,Preco) VALUES("{produto}",{preco})')
-conexao.commit()
-comand.execute('SELECT * FROM Produtos')
-for linha in comand.fetchall():
-    print(linha)
+    comand.execute(f'INSERT INTO Produtos(Produto,Preco) VALUES("{produto}",{preco})')
+    conexao.commit()
+    comand.execute('SELECT * FROM Produtos')
+    for linha in comand.fetchall():
+        print(linha)
 
+elif escolha==2:
+    produto= (input("Insira nome produto: "))
+    preco= (input("Insira preço do produto: "))
+    comand.execute(f'UPDATE Produtos SET Produto = "Bolonhesa" WHERE Produto = "Bosta"')
+    conexao.commit()
+    comand.execute('SELECT * FROM Produtos')
+    for linha in comand.fetchall():
+        print(linha)
 
-comand.execute(f'UPDATE Produtos SET Produto = "Bolonhesa" WHERE Produto = "Bosta"')
-conexao.commit()
-comand.execute('SELECT * FROM Produtos')
-for linha in comand.fetchall():
-    print(linha)
+elif escolha==3:
+    produto= (input("Insira nome produto: "))
+    preco= (input("Insira preço do produto: "))
+    comand.execute(f'DELETE FROM Produtos WHERE Produto = "Bosta"')
+    conexao.commit()
+    comand.execute('SELECT * FROM Produtos')
+    for linha in comand.fetchall():
+        print(linha)
 
 comand.close() #NAO ESQUEÇA DE FECHAR O CURSOR E A CONEXAO NO FINAL DO CODIGO
 conexao.close()
